7f6e65a0187183072bdbfae9667369fd491a2b1b
Giovannytrotta <giovannytrotta13@gmail.com>
2025-03-22 13:46:57 +0000

    change

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [activeLight, setActiveLight] = useState("red");
+useEffect(() => {
+const interval = setInterval(() => {
+  setActiveLight((prevLight) => {
+    if (prevLight === "red")return  "yellow"
+    if (prevLight === "yellow")return  "green"
+   return "red";
+  })
+}, 2000);
+return () => clearInterval(interval);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+}, []);
+
+
+
+
+	return (
+		<div className="d-flex flex-column align-items-center mt-5">
+      <div className="traffic-light bg-dark p-3 rounded d-flex flex-column">
+        <div
+          className={`light bg-danger rounded-circle ${activeLight === "red" ? "shadow-lg" : "opacity-50"}`}
+          onClick={() => setActiveLight("red")}
+        ></div>
+        <div
+          className={`light bg-warning rounded-circle my-2 ${activeLight === "yellow" ? "shadow-lg" : "opacity-50"}`}
+          onClick={() => setActiveLight("yellow")}
+        ></div>
+        <div
+          className={`light bg-success rounded-circle ${activeLight === "green" ? "shadow-lg" : "opacity-50"}`}
+          onClick={() => setActiveLight("green")}
+        ></div>
+      </div>
+    </div>
 	);
-};
+  };
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
@@ -11,8 +12,10 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+    <Home />
+  </React.StrictMode>
+);
